8a6a6b1ca9ab06164b62cd53ece2836b66ef4a75
josnachan <jyothsnachahanapally@gmail.com>
2026-01-19 10:16:29 -0800

    add num3

--- src/Addition.java
+++ src/Addition.java
@@ -1,13 +1,13 @@
 public class Addition {
     static void main(String[] args) {
-        int num1 = Integer.parseInt(args[]);
-        int num2 = Integer.parseInt(args[]);
-
-        add(num1, num2);
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int num3 = Integer.parseInt(args[2]);
+        add(num1, num2, num3);
 
     }
-    static void add(int num1, int num2) {
-        System.out.println(num1 + num2);
+    static void add(int num1, int num2, int num3) {
+        System.out.println(num1 + num2 + num3);
     }
 
 }
